a1c64379f2e081c69f41100057393f46f7fea5bb
evgeniy-mh <djpon94@gmail.com>
2017-04-14 23:10:20 +0300

    базовый gui

---
+++ src/main/java/com/evgeniy_mh/simpleaescipher/MainController.java
@@ -0,0 +1,18 @@
+package com.evgeniy_mh.simpleaescipher;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+public class MainController implements Initializable {
+    
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+}
