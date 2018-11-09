655ae258349e34a1e66e74e698e7d37859710f6d
evgeniy <evgeniy@evgeniy-G6PC>
2018-11-09 20:22:57 +0300

    fixed CBC encryption and decryption

--- src/main/java/com/evgeniy_mh/simpleaescipher/AESEngine/AES_CBCEncryptor.java
+++ src/main/java/com/evgeniy_mh/simpleaescipher/AESEngine/AES_CBCEncryptor.java
@@ -1,9 +1,11 @@
 package com.evgeniy_mh.simpleaescipher.AESEngine;
 
 import com.evgeniy_mh.simpleaescipher.CommonUtils;
+import com.evgeniy_mh.simpleaescipher.FileUtils;
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
 import java.util.Random;
 import javafx.concurrent.Task;
 import javafx.scene.control.ProgressIndicator;
@@ -31,7 +33,7 @@ public class AES_CBCEncryptor extends Encryptor {
 
                 try {
                     RandomAccessFile OUTraf = new RandomAccessFile(out, "rw");
-                    OUTraf.setLength(8 + in.length());
+                    OUTraf.setLength(IV.length + in.length());
                     OUTraf.write(IV);
 
                     RandomAccessFile INraf = new RandomAccessFile(in, "r");
@@ -64,16 +66,16 @@ public class AES_CBCEncryptor extends Encryptor {
                             for (int j = 0; j < AES.BLOCK_SIZE; j++) {
                                 k[j] = (byte) (IV[j] ^ temp[j]);
                             }
-                        } else {
+                        } else { //последующие итерации
                             for (int j = 0; j < AES.BLOCK_SIZE; j++) {
                                 k[j] = (byte) (prev[j] ^ temp[j]);
                             }
                         }
-
                         mAES.encrypt(k, prev);
-                        OUTraf.write(k);
+                        OUTraf.write(prev);
                         progressIndicator.setProgress((double) i / nBlocks);
                     }
+
                     INraf.close();
                     OUTraf.close();
                 } catch (IOException e) {
@@ -88,7 +90,67 @@ public class AES_CBCEncryptor extends Encryptor {
     @Override
 
     public Task decrypt(File in, File out, byte[] key) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+        return new Task<Void>() {
+            @Override
+            protected Void call() throws IOException {
+                byte[] IV = FileUtils.readBytesFromFile(in, AES.BLOCK_SIZE);
 
+                byte[] tempKey = key;
+                if (key.length % AES.BLOCK_SIZE != 0) {
+                    tempKey = PKCS7.PKCS7(key);
+                }
+                mAES.makeKey(tempKey, 128, AES.DIR_BOTH);
+
+                try {
+                    RandomAccessFile OUTraf = new RandomAccessFile(out, "rw");
+                    OUTraf.setLength(in.length() - IV.length);
+                    RandomAccessFile INraf = new RandomAccessFile(in, "r");
+
+                    int nBlocks = CommonUtils.countBlocks(in, AES.BLOCK_SIZE); //сколько блоков шифро текста
+                    int nToDeleteBytes = 0; //сколько байт нужно удалить с конца сообщения
+
+                    byte[] temp = new byte[AES.BLOCK_SIZE];
+                    byte[] prev = new byte[AES.BLOCK_SIZE];
+                    for (int i = 1; i < nBlocks; i++) {
+                        INraf.seek(i * 16); //установка указателя для считывания файла
+                        INraf.read(temp, 0, AES.BLOCK_SIZE); //считывание блока в temp
+
+                        byte[] k = new byte[AES.BLOCK_SIZE]; // k_i
+                        byte[] c = new byte[AES.BLOCK_SIZE]; //c_i
+
+                        mAES.decrypt(temp, k);
+
+                        if (i == 1) { //первая итерация
+                            for (int j = 0; j < AES.BLOCK_SIZE; j++) {
+                                c[j] = (byte) (IV[j] ^ k[j]);
+                            }
+                            System.arraycopy(temp, 0, prev, 0, AES.BLOCK_SIZE);
+                        } else {
+                            for (int j = 0; j < AES.BLOCK_SIZE; j++) {
+                                c[j] = (byte) (prev[j] ^ k[j]);
+                            }
+                        }
+                        System.arraycopy(temp, 0, prev, 0, AES.BLOCK_SIZE);
+                        OUTraf.write(c);
+
+                        if ((i + 1) == nBlocks) {
+                            if (c[AES.BLOCK_SIZE - 1] > 0 && c[AES.BLOCK_SIZE - 1] <= 16) {
+                                nToDeleteBytes = c[AES.BLOCK_SIZE - 1]; //на случай дешифрования с неправильным ключем
+                            }
+                        }
+                        progressIndicator.setProgress((double) i / nBlocks);
+                    }
+
+                    OUTraf.setLength(OUTraf.length() - nToDeleteBytes);
+                    OUTraf.close();
+                    INraf.close();
+                } catch (IOException e) {
+                    CommonUtils.reportExceptionToMainThread(e, "Exception in decrypt thread!");
+                }
+                progressIndicator.setProgress(0.0);
+                return null;
+            }
+        };
+
+    }
 }
