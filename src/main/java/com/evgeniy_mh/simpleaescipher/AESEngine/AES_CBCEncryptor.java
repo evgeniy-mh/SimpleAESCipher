f4b46d606659f05e9db072196c827808b7095a2b
evgeniy <evgeniy@evgeniy-G6PC>
2019-03-17 18:42:47 +0300

    Add russian comments

--- src/main/java/com/evgeniy_mh/simpleaescipher/AESEngine/AES_CBCEncryptor.java
+++ src/main/java/com/evgeniy_mh/simpleaescipher/AESEngine/AES_CBCEncryptor.java
@@ -20,66 +20,90 @@ public class AES_CBCEncryptor extends Encryptor {
         return new Task<Void>() {
             @Override
             protected Void call() throws IOException {
+                //Создание блока вектора инициализации 
+                //с_0=IV
                 Random random = new Random();
-                byte[] IV = new byte[AES.BLOCK_SIZE];
+                byte[] IV = new byte[AES.BLOCK_SIZE];                
                 random.nextBytes(IV);
 
+                //Если длина ключа не кратна длине блока(16 байт), то он дополняется  PKCS7
                 byte[] tempKey = key;
                 if (key.length % AES.BLOCK_SIZE != 0) {
                     tempKey = PKCS7.PKCS7(key);
                 }
+                //mAES — Объект класса AES. В него передается ключ, его длина и направление шифрования
                 mAES.makeKey(tempKey, 128, AES.DIR_BOTH);
 
                 try {
+                    //Открытие файла для записи результата шифрования
                     RandomAccessFile OUTraf = new RandomAccessFile(out, "rw");
+                    //Установка длины результирующего файла
                     OUTraf.setLength(IV.length + in.length());
+                    //Запись нулевого блока (вектора инициализации)
                     OUTraf.write(IV);
 
+                    //Открытие исходного файла
                     RandomAccessFile INraf = new RandomAccessFile(in, "r");
 
-                    int nBlocks = CommonUtils.countBlocks(in, AES.BLOCK_SIZE); //сколько блоков открытого текста
-                    byte[] temp = new byte[AES.BLOCK_SIZE]; //считываемый блок исходного текста
-                    byte[] prev = new byte[AES.BLOCK_SIZE]; //предыдущий зашифрованный блок
+                    //Подсчет количества блоков открытого текста
+                    int nBlocks = CommonUtils.countBlocks(in, AES.BLOCK_SIZE); 
+                    //Считываемый блок исходного текста
+                    byte[] temp = new byte[AES.BLOCK_SIZE]; 
+                    //Предыдущий зашифрованный блок
+                    byte[] prev = new byte[AES.BLOCK_SIZE]; 
 
+                    //Главный цикл в котором происходит обработка блоков
                     for (int i = 0; i < nBlocks + 1; i++) {
-                        INraf.seek(i * 16); //установка указателя для считывания файла
+                        //Установка указателя для считывания файла
+                        INraf.seek(i * 16); 
 
                         if ((i + 1) == nBlocks + 1) { //последняя итерация
+                            //deltaToBlock — колчество недостающих байт до полного блока
                             int deltaToBlock = (int) (in.length() % AES.BLOCK_SIZE);
+                            //Если это количество больше нуля происходит PKCS7 дополнение блока
                             if (deltaToBlock > 0) {
                                 temp = new byte[deltaToBlock];
-                                INraf.read(temp, 0, deltaToBlock);  //считывание неполного блока в temp
+                                //Считывание неполного блока в temp
+                                INraf.read(temp, 0, deltaToBlock); 
                                 temp = PKCS7.PKCS7(temp);
+                            //Иначе создается блок со значение всех байт 16
                             } else {
                                 temp = new byte[AES.BLOCK_SIZE];
                                 for (int t = 0; t < AES.BLOCK_SIZE; t++) {
                                     temp[t] = (byte) AES.BLOCK_SIZE;
                                 }
                             }
+                        //Иначе происходит обычное считывание блока исходного текста в буфер temp
                         } else {
-                            INraf.read(temp, 0, AES.BLOCK_SIZE); //считывание блока в temp
+                            INraf.read(temp, 0, AES.BLOCK_SIZE);
                         }
 
                         byte[] k = new byte[AES.BLOCK_SIZE];
                         if (i == 0) { //первая итерация
                             for (int j = 0; j < AES.BLOCK_SIZE; j++) {
+                                //c_1=(IV XOR p_1)
                                 k[j] = (byte) (IV[j] ^ temp[j]);
                             }
                         } else { //последующие итерации
                             for (int j = 0; j < AES.BLOCK_SIZE; j++) {
+                                //c_i=(c_i-1 XOR p_i)
                                 k[j] = (byte) (prev[j] ^ temp[j]);
                             }
                         }
                         mAES.encrypt(k, prev);
+
+                        //Запись результата шифрования
                         OUTraf.write(prev);
+                        //Обновление элемента пользовательского интерфейса
                         progressIndicator.setProgress((double) i / nBlocks);
                     }
-
+                    //Закрытие файловых потоков
                     INraf.close();
                     OUTraf.close();
                 } catch (IOException e) {
                     CommonUtils.reportExceptionToMainThread(e, "Exception in encrypt thread!");
                 }
+                //Обновление элемента пользовательского интерфейса
                 progressIndicator.setProgress(0.0);
                 return null;
             }
@@ -92,35 +116,50 @@ public class AES_CBCEncryptor extends Encryptor {
         return new Task<Void>() {
             @Override
             protected Void call() throws IOException {
+                //Считывание вектора инициализации из входного файла
                 byte[] IV = FileUtils.readBytesFromFile(in, AES.BLOCK_SIZE);
 
                 byte[] tempKey = key;
+                //Если длина ключа не кратна длине блока(16 байт), то он дополняется  PKCS7
                 if (key.length % AES.BLOCK_SIZE != 0) {
                     tempKey = PKCS7.PKCS7(key);
                 }
+                //mAES - объект класса AES. В него передается ключ, его длина и направление шифрования.
                 mAES.makeKey(tempKey, 128, AES.DIR_BOTH);
 
                 try {
+                    //Открытие файла для записи результата дешифрования
                     RandomAccessFile OUTraf = new RandomAccessFile(out, "rw");
+                    //Установка длины файла
                     OUTraf.setLength(in.length() - IV.length);
+                    //Открытие зашифрованного файла
                     RandomAccessFile INraf = new RandomAccessFile(in, "r");
 
-                    int nBlocks = CommonUtils.countBlocks(in, AES.BLOCK_SIZE); //сколько блоков шифро текста
-                    int nToDeleteBytes = 0; //сколько байт нужно удалить с конца сообщения
+                    //Количество блоков открытого текста
+                    int nBlocks = CommonUtils.countBlocks(in, AES.BLOCK_SIZE); 
+                    //Количество байт, которые будут удалены с конца файла
+                    int nToDeleteBytes = 0;
 
+                    //В буфер  temp будут считываться блоки по 16 байт из зашифрованного файла
                     byte[] temp = new byte[AES.BLOCK_SIZE];
+                    //В буфере prev хранится предыдущий блок зашифрованного сообщения
                     byte[] prev = new byte[AES.BLOCK_SIZE];
+                    //Главный цикл, в котором происходит обработка блоков
                     for (int i = 1; i < nBlocks; i++) {
-                        INraf.seek(i * 16); //установка указателя для считывания файла
-                        INraf.read(temp, 0, AES.BLOCK_SIZE); //считывание блока в temp
+                        //Установка указателя для считывания файла
+                        INraf.seek(i * 16); 
+                        //Считывание блока в temp
+                        INraf.read(temp, 0, AES.BLOCK_SIZE); 
 
                         byte[] k = new byte[AES.BLOCK_SIZE]; // k_i
                         byte[] c = new byte[AES.BLOCK_SIZE]; //c_i
 
+                        //k_i=Dk(c_i)
                         mAES.decrypt(temp, k);
 
                         if (i == 1) { //первая итерация
                             for (int j = 0; j < AES.BLOCK_SIZE; j++) {
+                                //p_1=(IV XOR Dk(k_1))
                                 c[j] = (byte) (IV[j] ^ k[j]);
                             }
                             System.arraycopy(temp, 0, prev, 0, AES.BLOCK_SIZE);
--- src/main/java/com/evgeniy_mh/simpleaescipher/AESEngine/AES_CTREncryptor.java
+++ src/main/java/com/evgeniy_mh/simpleaescipher/AESEngine/AES_CTREncryptor.java
@@ -23,47 +23,62 @@ public class AES_CTREncryptor extends Encryptor{
         return new Task<Void>() {
             @Override
             protected Void call() throws IOException {
+                //Создание блоков байт для хранения nonce и счетчика(counter)
                 byte[] nonce = ByteBuffer.allocate(8).putInt(getNonce()).array();
                 byte[] counter = ByteBuffer.allocate(8).putInt(0).array();
-                byte[] nonceAndCounter = new byte[AES.BLOCK_SIZE]; //используется в раундах //nonceAndCounter: 0000nnnn|0000cccc
-                byte[] nonceAndCounterInfo = new byte[8]; //8 байт которые добавл в начало сообщения и несут инфу о nonce и counter //nonceAndCounterInfo: nnnncccc
+                //Блок байт, который будет использоваться в раундах(16 байт): 0000nnnn|0000cccc
+                byte[] nonceAndCounter = new byte[AES.BLOCK_SIZE]; 
+                //Блок байт, который будет  добавлен в начало зашифрованного файла: nnnncccc
+                byte[] nonceAndCounterInfo = new byte[8];
                 System.arraycopy(nonce, 0, nonceAndCounterInfo, 0, 4);
                 System.arraycopy(counter, 0, nonceAndCounterInfo, 4, 4);
 
+                //Если длина ключа не кратна длине блока(16 байт), то он дополняется  PKCS7
                 byte[] tempKey = key;
                 if (key.length % AES.BLOCK_SIZE != 0) {
                     tempKey = PKCS7.PKCS7(key);
                 }
+                //mAES - объект класса AES. В него передается ключ, его длина и направление шифрования.
                 mAES.makeKey(tempKey, 128, AES.DIR_BOTH);
                 try {
+                    //Открытие файла для записи результата шифрования
                     RandomAccessFile OUTraf = new RandomAccessFile(out, "rw");
                     OUTraf.setLength(8 + in.length());
 
+                    //Запись в него информации о nonce и счетчике
                     OUTraf.write(nonceAndCounterInfo);
-
+                    //Открытие исходного файла
                     RandomAccessFile INraf = new RandomAccessFile(in, "r");
-
-                    int nBlocks = CommonUtils.countBlocks(in, AES.BLOCK_SIZE); //сколько блоков открытого текста
+                    //Количество блоков открытого текста
+                    int nBlocks = CommonUtils.countBlocks(in, AES.BLOCK_SIZE); 
+                    //В буффер  temp будут считываться блоки по 16 байт из исходного файла
                     byte[] temp = new byte[AES.BLOCK_SIZE];
 
+                    //Главный цикл в котором происходит обработка блоков
                     for (int i = 0; i < nBlocks + 1; i++) {
                         INraf.seek(i * 16); //установка указателя для считывания файла
 
-                        if ((i + 1) == nBlocks + 1) { //последняя итерация
+                        //Если это последняя итерация
+                        if ((i + 1) == nBlocks + 1) { 
+                            //deltaToBlock — колчество недостающих байт до полного блока
                             int deltaToBlock = (int) (in.length() % AES.BLOCK_SIZE);
+                            //Если это количество больше нуля происходит PKCS7 дополнение блока
                             if (deltaToBlock > 0) {
                                 temp = new byte[deltaToBlock];
                                 INraf.read(temp, 0, deltaToBlock);  //считывание неполного блока в temp
                                 temp = PKCS7.PKCS7(temp);
+                            //Иначе создается блок со значение всех байт 16
                             } else {
                                 temp = new byte[AES.BLOCK_SIZE];
                                 for (int t = 0; t < AES.BLOCK_SIZE; t++) {
                                     temp[t] = (byte) AES.BLOCK_SIZE;
                                 }
                             }
+                        //Иначе происходит обычное считывание блока исходного текста в буфер temp
                         } else {
                             INraf.read(temp, 0, AES.BLOCK_SIZE); //считывание блока в temp
                         }
+                        //Заполнение блока счетчика и формирование блока байт, который будет использоваться в раундах: 0000nnnn|0000cccc
                         counter = ByteBuffer.allocate(8).putInt(i).array();
                         System.arraycopy(nonce, 0, nonceAndCounter, 4, 8);
                         System.arraycopy(counter, 0, nonceAndCounter, 12, 4);//nonceAndCounter: 0000nnnn|0000cccc
@@ -72,12 +87,15 @@ public class AES_CTREncryptor extends Encryptor{
                         mAES.encrypt(nonceAndCounter, k);
 
                         byte[] c = new byte[AES.BLOCK_SIZE]; //c_i
-                        for (int j = 0; j < AES.BLOCK_SIZE; j++) { //xor p_i и k_i
+                        for (int j = 0; j < AES.BLOCK_SIZE; j++) { //xor c_i=(p_i XOR k_i)
                             c[j] = (byte) (temp[j] ^ k[j]);
                         }
+                        //Запись результата шифрования
                         OUTraf.write(c);
+                        //Обновление элемента пользовательского интерфейса
                         progressIndicator.setProgress((double) i / nBlocks);
                     }
+                    //Закрытие файловых потоков
                     OUTraf.close();
                     INraf.close();
                 } catch (IOException e) {
@@ -94,34 +112,50 @@ public class AES_CTREncryptor extends Encryptor{
         return new Task<Void>() {
             @Override
             protected Void call() throws IOException {
-                byte[] nonceAndCounterInfo = new byte[8]; //8 байт которые добавл в начало сообщения и несут инфу о nonce и counter //nonceAndCounterInfo: nnnncccc        
+                //Считывание из начала файла блока байт, который был добавлен при шифровании: nnnncccc
+                byte[] nonceAndCounterInfo = new byte[8]; 
                 nonceAndCounterInfo = FileUtils.readBytesFromFile(in, 0, 8);
 
+                //Создание блоков байт для хранения nonce и счетчика(counter)
                 byte[] nonce = new byte[8];
                 byte[] counter = new byte[8];
+                //Копирование значений в эти блоки
                 System.arraycopy(nonceAndCounterInfo, 0, nonce, 0, 4);
                 System.arraycopy(nonceAndCounterInfo, 4, counter, 0, 4);
 
+                //Блок байт, который будет использоваться в раундах(16 байт): 0000nnnn|0000cccc
                 byte[] nonceAndCounter = new byte[AES.BLOCK_SIZE];
 
+                //Если длина ключа не кратна длине блока(16 байт), то он дополняется  PKCS7
                 byte[] tempKey = key;
                 if (key.length % AES.BLOCK_SIZE != 0) {
                     tempKey = PKCS7.PKCS7(key);
                 }
+                //mAES — Объект класса AES. В него передается ключ, его длина и направление шифрования
                 mAES.makeKey(tempKey, 128, AES.DIR_BOTH);
                 try {
+                    //Открытие файла для записи результата дешифрования
                     RandomAccessFile OUTraf = new RandomAccessFile(out, "rw");
+                    //Его длина устанавливается на 8 байт меньше чем длина зашифрованного файла
                     OUTraf.setLength(in.length() - 8);
+                    //Открытие зашифрованного файла
                     RandomAccessFile INraf = new RandomAccessFile(in, "r");
 
+                    //Количество блоков открытого текста
                     int nBlocks = CommonUtils.countBlocks(in, AES.BLOCK_SIZE); //сколько блоков шифро текста
+                    //Количество байт, которые будут удалены с конца файла
                     int nToDeleteBytes = 0; //сколько байт нужно удалить с конца сообщения
 
+                    //В буфер  temp будут считываться блоки по 16 байт из зашифрованного файла
                     byte[] temp = new byte[AES.BLOCK_SIZE];
+                    //Главный цикл, в котором происходит обработка блоков
                     for (int i = 0; i < nBlocks; i++) {
-                        INraf.seek(i * 16 + 8); //установка указателя для считывания файла
-                        INraf.read(temp, 0, AES.BLOCK_SIZE); //считывание блока в temp
+                        //Установка указателя для считывания файла
+                        INraf.seek(i * 16 + 8); 
+                        //Считывание из файла в буфер temp
+                        INraf.read(temp, 0, AES.BLOCK_SIZE);
 
+                        //Заполнение блока счетчика и формирование блока байт который будет использоваться в раундах: 0000nnnn|0000cccc
                         counter = ByteBuffer.allocate(8).putInt(i).array();
                         System.arraycopy(nonce, 0, nonceAndCounter, 4, 8);
                         System.arraycopy(counter, 0, nonceAndCounter, 12, 4);
@@ -131,20 +165,27 @@ public class AES_CTREncryptor extends Encryptor{
                         mAES.encrypt(nonceAndCounter, k);
 
                         byte[] c = new byte[AES.BLOCK_SIZE]; //c_i
-                        for (int j = 0; j < AES.BLOCK_SIZE; j++) { //xor p_i и k_i
-                            c[j] = (byte) (temp[j] ^ k[j]);
+                        for (int j = 0; j < AES.BLOCK_SIZE; j++) { 
+                            c[j] = (byte) (temp[j] ^ k[j]); //p_i=(c_i XOR k_i)
                         }
+                        //Запись результата дешифрования
                         OUTraf.write(c);
 
+                        //Если это последняя итерация
                         if ((i + 1) == nBlocks) {
+                            //На случай дешифрования с неправильным ключом
                             if (c[AES.BLOCK_SIZE - 1] > 0 && c[AES.BLOCK_SIZE - 1] <= 16) {
+                                //Количество байт, которые будут удалены с конца файла
                                 nToDeleteBytes = c[AES.BLOCK_SIZE - 1]; //на случай дешифрования с неправильным ключем
                             }
                         }
+                        //Обновление элемента пользовательского интерфейса
                         progressIndicator.setProgress((double) i / nBlocks);
                     }
+                    //Из расшифрованного файла удаляются байты, которыми он был дополнен при шифровании
                     OUTraf.setLength(OUTraf.length() - nToDeleteBytes);
 
+                    //Закрытие файловых потоков
                     OUTraf.close();
                     INraf.close();
                 } catch (IOException e) {
