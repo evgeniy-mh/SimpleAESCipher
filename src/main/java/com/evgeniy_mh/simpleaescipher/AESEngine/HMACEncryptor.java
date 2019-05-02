44b75fdac30f841b5a48bff08e70c1d8b7f25dcc
evgeniy <evgeniy@evgeniy-G6PC>
2019-05-02 12:24:15 +0300

    Add russian comments

--- src/main/java/com/evgeniy_mh/simpleaescipher/AESEngine/HMACEncryptor.java
+++ src/main/java/com/evgeniy_mh/simpleaescipher/AESEngine/HMACEncryptor.java
@@ -85,18 +85,26 @@ public class HMACEncryptor {
     }
 
     public byte[] getHMAC(byte[] in, byte[] key) {
+
+        //Подготовка ключа по алгоритму HMAC, получение k0
         byte[] tempkey = prepareKey(key);
+        //Получение блока Si
         byte[] Si = new byte[BLOCK_SIZE];
         for (int i = 0; i < BLOCK_SIZE; i++) {
             Si[i] = (byte) (tempkey[i] ^ ipad[i]);
         }
+        //Получение блока So
         byte[] So = new byte[BLOCK_SIZE];
         for (int i = 0; i < BLOCK_SIZE; i++) {
             So[i] = (byte) (tempkey[i] ^ opad[i]);
         }
+        //Склейка строки Si с сообщением M
         byte[] temp = CommonUtils.concat(Si, in);
+        //Применение хеш функции с строке полученой на предыдущем шаге
         temp = md5.digest(temp);
+        //Склейка строки So со строкой полученой на предыдущем шаге
         temp = CommonUtils.concat(So, temp);
+        //Применение хеш функции с строке полученой на предыдущем шаге
         temp = md5.digest(temp);
         return temp;
     }
@@ -108,19 +116,30 @@ public class HMACEncryptor {
      * @return Подготовленный ключ шифрования
      */
     private byte[] prepareKey(byte[] key) {
+        //Создание буфера для сохранения обработаного ключа
         byte[] resultKey = new byte[BLOCK_SIZE];
 
+
         if (key.length == BLOCK_SIZE) {
+            //Если длина ключа совпадает с длиной блока
             resultKey = key;
         } else if (key.length > BLOCK_SIZE) {
+            //Если длина ключа больше длины блока
+            //Применение хеш функции к ключу
             byte[] temp = md5.digest(key);
+
+            //Копирование значения хеш функции в буфер результата
             System.arraycopy(temp, 0, resultKey, 0, temp.length);
 
+            //Дополнение ключа нулями
             for (int i = temp.length; i < BLOCK_SIZE; i++) {
                 resultKey[i] = 0;
             }
         } else { //if(key.length < BLOCK_SIZE)
+            //Если ключ меньше длины блока
+            //Копирование ключа
             System.arraycopy(key, 0, resultKey, 0, key.length);
+            //Добавление нулей к правой части результата
             for (int i = key.length; i < BLOCK_SIZE; i++) {
                 resultKey[i] = 0;
             }
